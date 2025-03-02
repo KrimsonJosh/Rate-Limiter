c4a06d293fbc188a825f424a95b9c0f18f2174a4
KrimsonJosh <jumoinzad@gmail.com>
2025-03-02 16:34:41 -0600

    add basic rate limiter with redis

---
+++ rate_limiter.py
@@ -0,0 +1,35 @@
+import time 
+import redis 
+from flask import request, jsonify 
+
+redis_client = redis.Redis(host = 'localhost', port = 6379, db = 0, decode_respones = True)
+
+def rate_limiter(max_requests: int, window: int):
+    '''
+        Rate Limiting decorator Using redis fixed window counter
+        :max requests: max allowed requests within window
+        :window: time (in seconds)
+    '''
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            client_IP = request.remote_addr 
+            key = f"rate_limit{client_IP}"
+
+            # Current Request Count 
+            current_count = redis_client.get(key) 
+
+            if current_count is None:
+                # First request, initialize 
+                redis_client.setex(key, window, 1)
+            else:
+                current_count = int(current_count) 
+                if current_count >= max_requests:
+                    return jsonify({
+                        "error": "Rate Limit Exceeded",
+                        "retry_after": redis_client.ttl(key)
+                    }), 429 
+                redis_client.incr(key) # Increments count 
+
+            return func(*args, **kwargs) 
+        return wrapper 
+    return decorator
